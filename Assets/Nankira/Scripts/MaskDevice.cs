6feba7cef660f079449335401c5dfdf3eece85f3
Ryotan1ff <87265930+Ryotan1ff@users.noreply.github.com>
2025-06-16 20:05:26 +0900

    デバイス定義クラスの追加
    実装はまだ

---
+++ Assets/Nankira/Scripts/MaskDevice.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using R3;
+
+namespace Nankira.Devices
+{
+    /// <summary>
+    /// マスクデバイス
+    /// </summary>
+    public sealed class MaskDevice
+    {
+        private List<Vibrator> _vibrators;
+        private ReactiveProperty<float> _pressureLeft;
+        private ReactiveProperty<float> _pressureRight;
+
+        /// <summary>
+        /// 左の圧力 .Valueで値を取得
+        /// </summary>
+        public ReadOnlyReactiveProperty<float> PressureLeft =>
+            _pressureLeft.ToReadOnlyReactiveProperty();
+
+        /// <summary>
+        /// 右の圧力 .Valueで値を取得
+        /// </summary>
+        public ReadOnlyReactiveProperty<float> PressureRight =>
+            _pressureRight.ToReadOnlyReactiveProperty();
+
+        // Singleton
+        private static readonly MaskDevice _instance = new();
+        public static MaskDevice Instance => _instance;
+
+        private MaskDevice()
+        {
+            // TODO: 振動子の初期化
+        }
+
+        // TODO: ここでデバイスの値をReactiveProperty.Valueにセットする処理を実装する
+        // 必要に応じてSingletonMonoBehaviourを継承
+    }
+}
---
+++ Assets/Nankira/Scripts/Vibrator.cs
@@ -0,0 +1,18 @@
+﻿namespace Nankira.Devices
+{
+    /// <summary>
+    /// 振動子
+    /// </summary>
+    public class Vibrator
+    {
+        public Vibrator()
+        {
+            // TODO: 実装
+        }
+
+        public void Play( /*wavファイル（型不明）*/)
+        {
+            // TODO: 実装
+        }
+    }
+}
