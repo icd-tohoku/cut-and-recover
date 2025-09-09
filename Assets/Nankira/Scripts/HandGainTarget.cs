938fb3e1338b116793f9d1cfaa239d2a01607470
Yugo <kigo1674@outlook.com>
2025-09-10 00:35:35 +0900

    ゲイン調整，それなりに実際の手の動きと連動するようになった

--- Assets/Nankira/Scripts/HandGainTarget.cs
+++ Assets/Nankira/Scripts/HandGainTarget.cs
@@ -7,8 +7,14 @@ public class HandGainTarget : MonoBehaviour
     [SerializeField] Transform _referencePoint;
 
     [Header("ゲイン値")]
-    [SerializeField] float _positionGain = 1.5f;
+    [SerializeField] Vector3 _positionGain;
     [SerializeField] float _rorationGain = 1.0f;
+
+    Vector3 _refPos;
+    Vector3 _delta;
+    float _geinedPos_x;
+    float _geinedPos_y;
+    float _geinedPos_z;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,9 +27,14 @@ public class HandGainTarget : MonoBehaviour
     {
         Debug.Assert( _handPoint != null && _referencePoint != null );
 
-        Vector3 _refPos = _referencePoint.position;
-        Vector3 _delta = _handPoint.position - _refPos;
-        transform.position = _refPos + _delta * _positionGain;
+        _refPos = _referencePoint.position;
+        _delta = _handPoint.position - _refPos;
+
+        _geinedPos_x = _refPos.x + _delta.x * _positionGain.x;
+        _geinedPos_y = _refPos.y + _delta.y * _positionGain.y;
+        _geinedPos_z = _refPos.z + _delta.z * _positionGain.z;
+
+        transform.position = new Vector3(_geinedPos_x, _geinedPos_y, _geinedPos_z);
 
         transform.rotation = Quaternion.Slerp(_referencePoint.rotation, _handPoint.rotation, _rorationGain);
     }
