8caa02efb29684a2931f705fd9c7658ba0cbb2df
Sato196 <sato.ikuru.s1@dc.tohoku.ac.jp>
2025-09-14 22:57:37 +0900

    めり込み補正とトラッキング失敗時の調整

--- Assets/Nankira/Scripts/HandGainTarget.cs
+++ Assets/Nankira/Scripts/HandGainTarget.cs
@@ -1,9 +1,15 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
+using System.Collections.Generic;
+using UnityEngine.UIElements;
+using System.Drawing;
 
 public class HandGainTarget : MonoBehaviour
 {
     [Header("ゲイン前の手")]
     [SerializeField] Transform _handPoint;
+    public OVRHand handtracked;
 
     [System.Serializable]
     public class GainInfo
@@ -12,7 +18,8 @@ public class HandGainTarget : MonoBehaviour
         public Transform referencePoint;
         public Vector3 positionGain = Vector3.one;
 
-        [Header("距離でゲインを有効化する場合")]
+        [Header("ある点からの距離でゲインを有効化する場合")]
+        public Transform headpoint;
         public bool useDistanceGate = false;
         public float enableDistance = 1.0f;
         public float distance = 1.0f;
@@ -22,6 +29,8 @@ public class HandGainTarget : MonoBehaviour
 
     void Update()
     {
+        // ハンドトラッキングができていない、または信頼性が低い場合は処理を行わない
+
         Debug.Assert(_handPoint != null, "Hand point is not assigned.");
 
         if (_gainInfos == null || _gainInfos.Length == 0)
@@ -37,6 +46,7 @@ public class HandGainTarget : MonoBehaviour
         foreach (var info in _gainInfos)
         {
             if (info == null) continue;
+            /*
 
             // 距離ゲート
             if (info.useDistanceGate)
@@ -44,7 +54,7 @@ public class HandGainTarget : MonoBehaviour
                 float dis = Vector3.Distance(_handPoint.position, info.referencePoint.position);
                 info.distance = dis;
                 if (dis > info.enableDistance) continue;
-            }
+            }*/
 
             // ベース位置用（平均）
             basePos += info.referencePoint.position;
@@ -60,6 +70,28 @@ public class HandGainTarget : MonoBehaviour
         basePos /= baseCount; // 平均
 
         // 3) 最終位置 = ベース位置 + 合成オフセット
-        transform.position = basePos + totalOffset;
+        Vector3 calculatedPosition = basePos + totalOffset;
+
+        if (!handtracked.IsTracked || !handtracked.IsDataHighConfidence)
+        {
+            calculatedPosition = transform.position;
+           
+        }
+
+        // 4) めり込み防止処理                                                                                                                                                                                                                           
+        foreach (var info in _gainInfos)
+        {
+            if (info != null && info.useDistanceGate)
+            {
+                float dis = Vector3.Distance(calculatedPosition, info.headpoint.position);
+                if (dis < info.enableDistance)
+                {
+                    Vector3 direction = (calculatedPosition - info.headpoint.position).normalized; 
+                    if (direction == Vector3.zero) direction = Vector3.up; // 念のため                                                                                                                                                                   
+                    calculatedPosition = info.headpoint.position + direction * info.enableDistance;
+                }
+            }
+        }
+        transform.position = calculatedPosition;
     }
 }
